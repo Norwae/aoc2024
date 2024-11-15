427e7e4c38891b96c05592e32bb14272b5e4b8b5
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-11-15 12:36:35 +0100

    Keep track of per-day timing even in optimized

--- src/ui/console.rs
+++ src/ui/console.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::io::{stdout, Stdout};
 use std::process::ExitCode;
 use std::sync::mpsc::{channel, Sender};
@@ -24,18 +25,20 @@ pub fn console_run(config: Configuration) -> ExitCode {
     ExitCode::SUCCESS
 }
 
-fn execute_day_handler(function: fn(&str, &mut Vec<u8>), input: String, out: Sender<OptimizedOutput>) {
+fn execute_day_handler(day: usize, function: fn(&str, &mut Vec<u8>), input: String, out: Sender<OptimizedOutput>) {
     let mut output_buffer = Vec::new();
     let start = Instant::now();
     function(&input, &mut output_buffer);
     let timing = Instant::now() - start;
     out.send(OptimizedOutput {
+        day,
         timing,
         output_buffer,
     }).unwrap()
 }
 
 struct OptimizedOutput {
+    day: usize,
     output_buffer: Vec<u8>,
     timing: Duration,
 }
@@ -46,7 +49,8 @@ pub fn optimized_run(config: Configuration) -> ExitCode {
     let mut expected_answers = 0;
     let (sender, receive) = channel();
     for day in config.active_days() {
-        let index = *day - 1;
+        let day = *day;
+        let index = day - 1;
         if let Some(callbacks) = &HANDLERS[index] {
             let sender = sender.clone();
             let handler = if config.verbose {
@@ -57,7 +61,7 @@ pub fn optimized_run(config: Configuration) -> ExitCode {
             let input = config.load_input(index + 1);
             expected_answers += 1;
             run_on_worker(move || {
-                execute_day_handler(handler, input, sender)
+                execute_day_handler(day, handler, input, sender)
             })
         }
     }
@@ -65,15 +69,25 @@ pub fn optimized_run(config: Configuration) -> ExitCode {
 
     let mut total_duration = Duration::ZERO;
     let mut overall_output = String::new();
+    let mut day_eval_timings = String::new();
     for _ in 0..expected_answers {
-        let OptimizedOutput { timing, output_buffer } = receive.recv().expect("Received answer");
+        let OptimizedOutput { day, timing, output_buffer } = receive.recv().expect("Received answer");
         total_duration += timing;
         overall_output += &String::from_utf8(output_buffer).expect("Valid utf8");
+        day_eval_timings += &format!("Day {day}: {timing:?}")
     }
 
     let clock_duration = Instant::now() - clock_start;
 
-    println!("Overall run complete. Wall time: {clock_duration:?}, Sum Task CPU time: {total_duration:?}, Output: \n{overall_output}");
+    println!("Overall run complete.
+Wall time: {clock_duration:?}
+Sum Task CPU time: {total_duration:?}
+
+Output:
+{overall_output}
+
+Day evaluation times:
+{day_eval_timings}");
 
     ExitCode::SUCCESS
 }
