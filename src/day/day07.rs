7798b31b750095746b8c02a9eb91491d0f1fb5ca
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-11-25 10:24:15 +0100

    Day 7 - 0 copy

--- src/day/day07.rs
+++ src/day/day07.rs
@@ -19,26 +19,61 @@ fn solve<T: UIWrite>(input: &str, out: &mut T) -> String {
     part1(&mut parsed);
     let root = parsed.root.unwrap();
     let start_part_2 = Instant::now();
-    out.info(format_args!("Part 1 -> {}: {:?}", root.name, start_part_2 - start_part_1));
-
-
+    let required_weight = part2(&root);
+    out.info(format_args!("Part 1 -> {}; Part 2 -> {}: {:?}", root.name, required_weight, start_part_2 - start_part_1));
     out.info(format_args!("Done: {:?}", start_part_2 - start_parse));
-    format!("{}", root.name)
-
+    format!("{} {}", root.name, required_weight)
 }
 
-fn part1<'a>(input: &'a mut Day7) {
+fn find_mismatch_in<'input, 'nodes>(nodes: &'nodes Vec<Day7Node<'input>>, mut path_accu: Vec<&'nodes Day7Node<'input>>) -> Vec<&'nodes Day7Node<'input>> {
+    let mismatched_node = nodes.as_slice().windows(3).find_map(|w| {
+        if let [a, b, c] = w {
+            if a.cumulative_weight != b.cumulative_weight {
+                if b.cumulative_weight != c.cumulative_weight {
+                    Some(b)
+                } else {
+                    Some(a)
+                }
+            } else if b.cumulative_weight != c.cumulative_weight {
+                Some(c)
+            } else {
+                None
+            }
+        } else {
+            panic!("Pattern match failed for {:?}", w)
+        }
+    });
+
+    if let Some(mismatched_node) = mismatched_node {
+        path_accu.push(mismatched_node);
+        path_accu = find_mismatch_in(&mismatched_node.children, path_accu)
+    }
+
+    path_accu
+}
+
+fn part2(root: &Day7Node) -> usize {
+    let mismatch_node_path = find_mismatch_in(&root.children, vec![root]);
+    let wrong_weight_node = mismatch_node_path[mismatch_node_path.len() - 1];
+    let child_sum = wrong_weight_node.children.iter().map(|c|c.cumulative_weight).sum::<usize>();
+    let wrong_weight_sibling = mismatch_node_path[mismatch_node_path.len() - 2].children.iter().find(|it|it.name != wrong_weight_node.name).unwrap();
+    let reference_weight = wrong_weight_sibling.cumulative_weight;
+    let required_self_weight = reference_weight - child_sum;
+    required_self_weight
+}
+
+fn part1(input: &mut Day7) {
     let mut unresolved = HashMap::new();
-    let mut next_spec = input.node_specs.iter().map(|(name, _)|*name).next();
+    let mut next_spec = input.node_specs.iter().map(|(name, _)| *name).next();
     while let Some(name) = next_spec {
         let next = input.node_specs.remove(name).unwrap();
         let next_node = next.resolve(&mut input.node_specs, &mut unresolved);
         unresolved.insert(next_node.name, next_node);
-        next_spec = input.node_specs.iter().map(|(name, _)|*name).next();
+        next_spec = input.node_specs.iter().map(|(name, _)| *name).next();
     }
 
     let mut iter = unresolved.into_iter();
-    input.root = iter.next().map(|(_, d)|d);
+    input.root = iter.next().map(|(_, d)| d);
     assert!(iter.next().is_none());
 }
 
@@ -52,24 +87,30 @@ struct NodeSpec<'a> {
 impl<'a> NodeSpec<'a> {
     fn resolve(self, other_specs: &mut HashMap<&'a str, NodeSpec<'a>>, unresolved_nodes: &mut HashMap<&'a str, Day7Node<'a>>) -> Day7Node<'a> {
         let NodeSpec { name, weight, mut unresolved_child_names } = self;
+        let self_weight = weight;
         let mut children = Vec::new();
+        let mut cumulative_weight = weight;
         while let Some(child) = unresolved_child_names.pop() {
-            if let Some(dangling_node) = unresolved_nodes.remove(child){
+            if let Some(dangling_node) = unresolved_nodes.remove(child) {
+                cumulative_weight += dangling_node.cumulative_weight;
                 children.push(dangling_node)
             } else {
                 let recurse_spec = other_specs.remove(child).expect("Referenced node exists");
-                children.push(recurse_spec.resolve(other_specs, unresolved_nodes))
+                let child = recurse_spec.resolve(other_specs, unresolved_nodes);
+                cumulative_weight += child.cumulative_weight;
+                children.push(child)
             }
         }
 
-        Day7Node { name, weight, children }
+        Day7Node { name, self_weight, cumulative_weight, children }
     }
 }
 
 #[derive(Debug)]
 struct Day7Node<'a> {
     name: &'a str,
-    weight: usize,
+    self_weight: usize,
+    cumulative_weight: usize,
     children: Vec<Day7Node<'a>>,
 }
 
@@ -92,15 +133,15 @@ fn node_spec(input: &str) -> IResult<&str, NodeSpec> {
         opt(child_list)
     )), |(name, _, weight, _, children)| {
         let weight = weight.parse().unwrap();
-        let children = children.unwrap_or_default();
-        NodeSpec { name, weight, unresolved_child_names: children }
+        let unresolved_child_names = children.unwrap_or_default();
+        NodeSpec { name, weight, unresolved_child_names }
     })(input)
 }
 
 fn parse(input: &str) -> IResult<&str, Day7> {
     let (rest, node_specs) = separated_list1(line_ending, node_spec)(input)?;
     let node_specs = node_specs.into_iter()
-        .map(|spec|(spec.name, spec))
+        .map(|spec| (spec.name, spec))
         .collect();
     Ok((rest, Day7 {
         node_specs,
