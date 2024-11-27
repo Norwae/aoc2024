f53053dd493db98996c57d180a260f38fb2d9cc4
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-11-27 10:32:03 +0100

    day 14 faster

--- src/day/day14.rs
+++ src/day/day14.rs
@@ -2,19 +2,13 @@ use crate::*;
 use crate::day::day10::KnotHash;
 use crate::vec2d::{CompassDirection, Index2D, Vec2D};
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct RegionAssignment {
-    filled: bool,
-    assigned: bool
-}
-
 simple_day!(|i|solve(i));
 fn solve(input: &str) -> String {
     let input = input.trim();
     let mut used = 0;
     let mut expanded_bit_field = Vec2D::new(128);
     for n in 0..128{
-        let mut buffer = [RegionAssignment {filled: false, assigned: true}; 128];
+        let mut buffer = [false; 128];
         let hash = KnotHash::hash(format!("{}-{}", input, n).as_bytes());
         used += hash.count_ones();
         expand_i128_to_bool_array(hash, &mut buffer);
@@ -22,7 +16,7 @@ fn solve(input: &str) -> String {
     }
     
     let mut region_count = 0;
-    while let Some(index) = expanded_bit_field.find(&RegionAssignment { filled: true, assigned: false}) {
+    while let Some(index) = expanded_bit_field.find(&true) {
         region_count += 1;
         mark_region(&mut expanded_bit_field, index)
     }
@@ -30,15 +24,15 @@ fn solve(input: &str) -> String {
     format!("Part1: {}, Part2: {}", used, region_count)
 }
 
-fn mark_region(regions: &mut Vec2D<RegionAssignment>, initial: Index2D) {
+fn mark_region(regions: &mut Vec2D<bool>, initial: Index2D) {
     let mut queue = vec![initial];
 
     while let Some(next) = queue.pop() {
-        if regions[next].assigned {
+        if !regions[next] {
             continue;
         }
 
-        regions[next].assigned = true;
+        regions[next] = false;
 
         for d in CompassDirection::ALL {
             let spread_to = next + d;
@@ -49,11 +43,9 @@ fn mark_region(regions: &mut Vec2D<RegionAssignment>, initial: Index2D) {
     }
 }
 
-fn expand_i128_to_bool_array(mut value: u128, target: &mut [RegionAssignment; 128]) {
+fn expand_i128_to_bool_array(mut value: u128, target: &mut [bool; 128]) {
     for i in 0..128  {
-        if (value & 0x8000_0000_0000_0000_0000_0000_0000_0000) != 0 {
-            target[i] = RegionAssignment { filled: true, assigned: false};
-        }
+        target[i] = (value & 0x8000_0000_0000_0000_0000_0000_0000_0000) != 0;
         value <<= 1
     }
 }
