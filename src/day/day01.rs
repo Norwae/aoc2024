9d727dbcafcc9bfdba8caeef3f0d09caa0fae1d6
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-12-02 10:08:19 +0100

    Trivial solutions for trivial days

--- src/day/day01.rs
+++ src/day/day01.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+use nom::character::complete::{line_ending, space1};
+use nom::multi::separated_list1;
+use nom::sequence::{separated_pair};
 use crate::*;
+use crate::day::nom_parsed;
+use crate::parse_helpers::parse_unsigned_nr;
 
-unimplemented_day!();
+parsed_day!(nom_parsed(
+    separated_list1(line_ending,
+        separated_pair(parse_unsigned_nr::<i32>, space1, parse_unsigned_nr::<i32>)
+    )),
+    |pairs|{
+        let (mut a, mut b): (Vec<i32>, Vec<i32>) = pairs.iter().cloned().unzip();
+        a.sort();
+        b.sort();
+
+        a.iter().zip(b.iter()).map(|(a, b)|(a - b).abs()).sum::<i32>()
+    },|pairs|{
+        let mut counts = HashMap::<i32, i32>::new();
+        for (_, r) in &pairs {
+            *counts.entry(*r).or_default() += 1;
+        }
+
+        pairs.into_iter().map(|(l,_)| l * counts.get(&l).cloned().unwrap_or_default()).sum::<i32>()
+    }
+);
--- src/day/day02.rs
+++ src/day/day02.rs
@@ -1,3 +1,82 @@
+use std::ops::{RangeBounds, RangeInclusive};
+use nom::character::complete::{line_ending, space1};
+use nom::multi::separated_list1;
 use crate::*;
+use crate::day::nom_parsed;
+use crate::parse_helpers::parse_unsigned_nr;
 
-unimplemented_day!();
+parsed_day!(
+    nom_parsed(
+        separated_list1(line_ending, separated_list1(space1, parse_unsigned_nr::<i32>))
+    ),
+    count_safe_lines,
+    count_safe_lines_with_tolerance
+);
+
+const ACCEPTABLE_DELTA: RangeInclusive<i32> = 1..=3;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Direction {
+    DESCENDING,
+    ASCENDING,
+}
+
+impl Direction {
+    fn from_difference(a: i32, b: i32) -> Option<Self> {
+
+        if b < a {
+            Some(Direction::DESCENDING)
+        } else if b > a {
+            Some(Direction::ASCENDING)
+        } else {
+            None
+        }
+    }
+}
+
+fn line_is_safe_simple(line: &[i32]) -> bool {
+    if line.len() < 2 {
+        return true;
+    }
+    let direction = Direction::from_difference(line[0], line[1]);
+    if direction.is_none() {
+        return false;
+    }
+    let direction = direction.unwrap();
+    for s in line.windows(2) {
+        let l = s[0];
+        let r = s[1];
+        let diff = if direction == Direction::DESCENDING {
+            l - r
+        } else {
+            r - l
+        };
+
+        if !ACCEPTABLE_DELTA.contains(&diff) {
+            return false;
+        }
+    }
+    true
+}
+
+fn line_is_safe_with_tolerance(line: &Vec<i32>) -> bool {
+    let mut skip_buffer = vec![0; line.len() - 1];
+    for i in 0..=line.len() - 1 {
+        (&mut skip_buffer[0..i]).copy_from_slice(&line[0..i]);
+        (&mut skip_buffer[i..]).copy_from_slice(&line[i+1..]);
+
+        if line_is_safe_simple(&skip_buffer) {
+            return true
+        }
+    }
+    false
+}
+
+
+fn count_safe_lines(input: &mut Vec<Vec<i32>>) -> usize {
+    input.iter().filter(|l| line_is_safe_simple(l)).count()
+}
+
+fn count_safe_lines_with_tolerance(input: Vec<Vec<i32>>) -> usize {
+    input.into_iter().filter(|l| line_is_safe_with_tolerance(l)).count()
+}
