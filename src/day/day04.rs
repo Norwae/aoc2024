80be26444772f4150a08bfe13190265901598eda
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-12-04 09:30:04 +0100

    Day 4

--- src/day/day04.rs
+++ src/day/day04.rs
@@ -2,19 +2,42 @@ use nom::character::complete::u32;
 use crate::*;
 use crate::vec2d::Index2D;
 
-const XMAS: u32 = 0x584D4153;
+const XMAS: &'static [u8;4] = b"XMAS";
+const SAMX: &'static [u8;4] = b"SAMX";
 
-struct Visor<'a, F> {
+const X_MAS_PATTERNS: [&'static [u8; 5]; 4] = [
+    /* M S
+        A
+       M S 
+     */
+    b"AMSMS",
+    /* M M
+        A
+       S S 
+     */
+    b"AMMSS",
+    /* S M
+        A
+       S M 
+     */
+    b"ASMSM",
+    /* S S
+        A
+       M M 
+     */
+    b"ASSMM",
+];
+
+struct Visor<'a> {
     bytes: &'a [u8],
     newline_at: usize,
-    out_of_bounds: F,
 }
 
-impl<'a, F: Fn(i32, i32, usize, &[u8]) -> u8> Visor<'a, F> {
-    fn new(bytes: &'a [u8], out_of_bounds: F) -> Self {
+impl<'a> Visor<'a> {
+    fn new(bytes: &'a [u8]) -> Self {
         let newline_at = bytes.iter().position(|it| *it == b'\n').unwrap();
 
-        Self { bytes, out_of_bounds, newline_at }
+        Self { bytes, newline_at }
     }
 
     fn columns(&self) -> usize {
@@ -27,7 +50,7 @@ impl<'a, F: Fn(i32, i32, usize, &[u8]) -> u8> Visor<'a, F> {
 
     fn at(&self, row: i32, column: i32) -> u8 {
         if row < 0 || column < 0 || column as usize >= self.columns() || row as usize >= self.rows() {
-            (&self.out_of_bounds)(row, column , self.newline_at, self.bytes)
+            b'!'
         } else {
             let offset = (self.newline_at + 1) * row as usize + column as usize;
             self.bytes[offset]
@@ -35,50 +58,80 @@ impl<'a, F: Fn(i32, i32, usize, &[u8]) -> u8> Visor<'a, F> {
     }
 }
 
-fn solve(input: &str) -> String {
-    let cutting_visor = Visor::new(input.as_bytes(), |_, _ , _, _| b'!');
+fn solve_1(visor: &Visor) -> usize {
     let mut bytes = [0u8; 4];
     let mut found = 0;
 
-    for row in 0..cutting_visor.rows() as i32 {
-        for column in 0..cutting_visor.columns() as i32 {
-            bytes[0] = cutting_visor.at(row, column);
-            bytes[1] = cutting_visor.at(row, column + 1);
-            bytes[2] = cutting_visor.at(row, column + 2);
-            bytes[3] = cutting_visor.at(row, column + 3);
+    for row in 0..visor.rows() as i32 {
+        for column in 0..visor.columns() as i32 {
+            bytes[0] = visor.at(row, column);
+            if bytes[0] != b'X' && bytes[0] != b'S' {
+                continue
+            }
 
-            if u32::from_be_bytes(bytes) == XMAS || u32::from_le_bytes(bytes) == XMAS {
+            bytes[1] = visor.at(row, column + 1);
+            bytes[2] = visor.at(row, column + 2);
+            bytes[3] = visor.at(row, column + 3);
+
+            if &bytes == XMAS || &bytes == SAMX {
                 found += 1;
             }
 
-            bytes[1] = cutting_visor.at(row + 1, column + 1);
-            bytes[2] = cutting_visor.at(row + 2, column + 2);
-            bytes[3] = cutting_visor.at(row + 3, column + 3);
+            bytes[1] = visor.at(row + 1, column + 1);
+            bytes[2] = visor.at(row + 2, column + 2);
+            bytes[3] = visor.at(row + 3, column + 3);
 
-            if u32::from_be_bytes(bytes) == XMAS || u32::from_le_bytes(bytes) == XMAS {
+            if &bytes == XMAS || &bytes == SAMX {
                 found += 1;
             }
 
 
-            bytes[1] = cutting_visor.at(row + 1, column);
-            bytes[2] = cutting_visor.at(row + 2, column);
-            bytes[3] = cutting_visor.at(row + 3, column);
+            bytes[1] = visor.at(row + 1, column);
+            bytes[2] = visor.at(row + 2, column);
+            bytes[3] = visor.at(row + 3, column);
 
-            if u32::from_be_bytes(bytes) == XMAS || u32::from_le_bytes(bytes) == XMAS {
+            if &bytes == XMAS || &bytes == SAMX {
                 found += 1;
             }
 
-            bytes[1] = cutting_visor.at(row + 1, column - 1);
-            bytes[2] = cutting_visor.at(row + 2, column - 2);
-            bytes[3] = cutting_visor.at(row + 3, column - 3);
+            bytes[1] = visor.at(row + 1, column - 1);
+            bytes[2] = visor.at(row + 2, column - 2);
+            bytes[3] = visor.at(row + 3, column - 3);
 
-            if u32::from_be_bytes(bytes) == XMAS || u32::from_le_bytes(bytes) == XMAS {
+            if &bytes == XMAS || &bytes == SAMX {
                 found += 1;
             }
         }
     }
 
-    format!("Part 1: {found}")
+    found
 }
 
-simple_day!(|i|solve(i));
+fn solve_2(visor: Visor) -> usize {
+    let mut bytes = [0u8; 5];
+    let mut found = 0;
+
+    for row in 0..visor.rows() as i32 {
+        'col: for column in 0..visor.columns() as i32 {
+            bytes[0] = visor.at(row + 1, column + 1);
+            if bytes[0] != b'A'  {
+                continue
+            }
+
+            bytes[1] = visor.at(row, column);
+            bytes[2] = visor.at(row, column + 2);
+            bytes[3] = visor.at(row + 2, column);
+            bytes[4] = visor.at(row + 2, column + 2);
+            for pattern in X_MAS_PATTERNS {
+                if &bytes == pattern {
+                    found += 1;
+                    continue 'col;
+                }
+            }
+        }
+    }
+
+    found
+}
+
+parsed_day!(|str|Ok::<_, !>(Visor::new(str.as_bytes())), |i|solve_1(i), solve_2);
