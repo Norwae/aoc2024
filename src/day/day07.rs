2ba0bcfad4ba6e43edb0a01275c1ec0d6bd0ad79
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-12-09 10:25:01 +0100

    Dirty day 7

--- src/day/day07.rs
+++ src/day/day07.rs
@@ -1,3 +1,67 @@
+use nom::bytes::complete::tag;
+use nom::character::complete::line_ending;
+use nom::combinator::map;
+use nom::IResult;
+use nom::multi::separated_list1;
+use nom::sequence::separated_pair;
 use crate::*;
+use crate::collections::ArrayBag;
+use crate::day::nom_parsed_bytes;
+use crate::parse_helpers::parse_unsigned_nr_bytes;
 
-unimplemented_day!();
+#[derive(Debug)]
+struct Problem {
+    target_value: u64,
+    operands: Vec<u64>,
+}
+
+struct Result {
+    success: bool,
+    concat_used: bool
+}
+
+fn concat(l: u64, r: u64) -> u64 {
+    let shifting = r.ilog10() + 1;
+    let shifted = l * 10_u64.pow(shifting);
+
+    shifted + r
+}
+
+fn can_solve(target_value: u64, accu: u64, rest: &[u64], used_concat: bool) -> bool {
+    if target_value < accu {
+        false
+    } else if rest.is_empty() {
+        accu == target_value
+    } else {
+        can_solve(target_value, accu * rest[0], &rest[1..], used_concat) ||
+            can_solve(target_value, accu + rest[0], &rest[1..], used_concat) ||
+            can_solve(target_value, concat(accu, rest[0]), &rest[1..], true)
+    }
+}
+
+
+fn parse(input: &[u8]) -> IResult<&[u8], Vec<Problem>> {
+    let parse_problem = map(
+        separated_pair(parse_unsigned_nr_bytes, tag(b": "), separated_list1(tag(b" "), parse_unsigned_nr_bytes)),
+        |(target_value, operands)| Problem { target_value, operands },
+    );
+    separated_list1(line_ending, parse_problem)(input)
+}
+
+fn solve(input: Vec<Problem>) -> String {
+    let mut sum_1 = 0;
+    let mut sum_2 = 0;
+
+    for problem in input {
+        if can_solve(problem.target_value, problem.operands[0], &problem.operands[1..], false) {
+            sum_1 += problem.target_value;
+            sum_2 += problem.target_value;
+        } else if can_solve(problem.target_value, problem.operands[0], &problem.operands[1..], true) {
+            sum_2 += dbg!(problem.target_value);
+        }
+    }
+
+    format!("{} - {}", sum_1, sum_2)
+}
+
+parsed_day!(nom_parsed_bytes(parse), solve);
