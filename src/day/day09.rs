bc1b696200c0852c39f3c8effc5522584e5ad5d8
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2024-12-09 15:18:51 +0100

    Parse into spans, then unfold

--- src/day/day09.rs
+++ src/day/day09.rs
@@ -2,33 +2,43 @@ use std::mem::swap;
 use crate::*;
 use crate::ui::UIWrite;
 
-simple_day!(solve);
+parsed_day!(parse, p1);
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum Mode {
-    FILE,
-    SPACE
+struct Span {
+    file_id: i32,
+    length: u8,
 }
+fn parse(input: &[u8]) -> Result<Vec<Span>, !> {
+    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+    enum Mode {
+        FILE,
+        SPACE,
+    }
 
-fn solve(input: &[u8], ui: &mut impl UIWrite) -> usize {
-    ui.info(format_args!("Got {} input bytes", input.len()));
-    let mut memory = Vec::new();
-    let mut file_id = 0;
+    let mut spans = Vec::new();
+    let mut next_id = 0;
     let mut mode = Mode::FILE;
     for next_byte in input {
-        let next_span = *next_byte - b'0';
-        for _ in 0..next_span {
-            memory.push(if mode == Mode::FILE { file_id } else { - 1 })
-        }
+        let length = *next_byte - b'0';
+        let file_id = if mode == Mode::FILE { next_id } else { -1 };
+        spans.push(Span { length, file_id });
 
         if mode == Mode::FILE {
             mode = Mode::SPACE;
         } else {
             mode = Mode::FILE;
-            file_id += 1;
+            next_id += 1;
         }
     }
+
+    Ok(spans)
+}
+
+fn p1(input: Vec<Span>) -> usize {
     let mut front = 0;
+    let mut memory: Vec<_> = input.iter().flat_map(|Span{length, file_id}|{
+        (0..*length).map(move |_|*file_id)
+    }).collect();
     let mut back = memory.len() - 1;
 
     while front < back {
@@ -45,7 +55,7 @@ fn solve(input: &[u8], ui: &mut impl UIWrite) -> usize {
 }
 
 fn checksum(memory: &[i32]) -> usize {
-    memory.iter().enumerate().map(|(n, v)|{
+    memory.iter().enumerate().map(|(n, v)| {
         if *v > 0 {
             n * (*v as usize)
         } else {
